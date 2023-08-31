07ded548823c3b5d01569f7b079882392021a31f
1evanter <rita.oleksenkoo@gmail.com>
2023-08-31 20:14:21 +0300

    ContactForm basic styles

---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { Form, Field } from 'formik';
+
+export const StyledForm = styled(Form)`
+max-width: 500px;
+display: flex;
+
+padding: 15px;
+gap: 15px;
+margin: 30px;
+// justify-content: space-between;
+align-items: center;
+border: 2px dashed black;
+`
+export const StyledField = styled(Field)`
+
+`
