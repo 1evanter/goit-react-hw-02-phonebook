5d57cfbf5a3507eba3faf66874e64a28fb7052a6
1evanter <rita.oleksenkoo@gmail.com>
2023-09-01 01:09:52 +0300

    Filter styles

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,11 @@
-export const Filter = ({onChange, value}) => {
+import { Input } from "./Filter.styled"
+import { Label } from "components/ContactForm/ContactForm.styled"
+
+export const Filter = ({ onChange, value }) => {
     return (
-        <label>
+        <Label>
             Find contacts by name
-        <input type="text" onChange={onChange} value={value} name="filter"/>
-        </label>
+        <Input type="text" onChange={onChange} value={value} name="filter"/>
+        </Label>
     )
 }
